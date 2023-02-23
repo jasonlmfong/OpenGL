b3ec843291596c0066275b936457cebc881d4892
Jason Fong <jasonlmfong@gmail.com>
2023-02-23 16:19:17 -0500

    abstract away draw and clear calls in renderer

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -9,6 +9,7 @@
 #include "Renderer.h"
 
 #include "VertexBuffer.h"
+#include "VertexBufferLayout.h"
 #include "IndexBuffer.h"
 #include "VertexArray.h"
 #include "Shader.h"
@@ -84,22 +85,21 @@ int main(void)
         ib.Unbind();
         shader.Unbind();
 
+        Renderer renderer;
+
         float g = 0.0f;
         float increment = 0.05f;
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
         {
             /* Render here */
-            GLCall(glClear(GL_COLOR_BUFFER_BIT));
+            renderer.Clear();
 
             shader.Bind();
             shader.SetUniform4f("u_Color", 0.26f, g, 0.96f, 1.0f);
 
-            va.Bind();
-            ib.Bind();
-
             /* Create a shape */
-            GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+            renderer.Draw(va, ib, shader);
 
             if (g > 1.0f)
                 increment = -0.5f;
