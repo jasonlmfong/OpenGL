3a903d6ef786919f654bfa783d80b8bb645fbe70
Jason Fong <jasonlmfong@gmail.com>
2023-02-26 22:33:35 -0500

    render multiple copies of the penguin

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -57,10 +57,10 @@ int main(void)
 
     {
         float positions[] = {
-            100.0f, 100.0f, 0.0f, 0.0f, // 0
-            200.0f, 100.0f, 1.0f, 0.0f, // 1
-            200.0f, 200.0f, 1.0f, 1.0f, // 2
-            100.0f, 200.0f, 0.0f, 1.0f  // 3
+            -50.0f, -50.0f, 0.0f, 0.0f, // 0
+             50.0f, -50.0f, 1.0f, 0.0f, // 1
+             50.0f,  50.0f, 1.0f, 1.0f, // 2
+            -50.0f,  50.0f, 0.0f, 1.0f  // 3
         };
 
         unsigned int indices[] = {
@@ -115,7 +115,8 @@ int main(void)
         ImGui::StyleColorsDark();
 
         //translate view
-        glm::vec3 translation(300, 200, 0);
+        glm::vec3 translation1(300, 200, 0);
+        glm::vec3 translation2(600, 300, 0);
 
         //color changing
         float g = 0.0f;
@@ -131,15 +132,25 @@ int main(void)
             ImGui_ImplGlfw_NewFrame();
             ImGui::NewFrame();
 
-            glm::mat4 view = glm::translate(glm::mat4(1.0f), translation);
-            glm::mat4 mvp = proj * view * model;
-
             shader.Bind();
             shader.SetUniform4f("u_Color", 0.26f, g, 0.96f, 1.0f);
-            shader.SetUniformMat4f("u_MVP", mvp);
 
-            /* bind va and ib, then create a shape */
-            renderer.Draw(va, ib, shader);
+            {
+                glm::mat4 view = glm::translate(glm::mat4(1.0f), translation1);
+                glm::mat4 mvp = proj * view * model;
+                shader.SetUniformMat4f("u_MVP", mvp);
+
+                /* bind va and ib, then create a shape */
+                renderer.Draw(va, ib, shader);
+            }
+
+            /* draw a second shape using a second MVP*/
+            {
+                glm::mat4 view = glm::translate(glm::mat4(1.0f), translation2);
+                glm::mat4 mvp = proj * view * model;
+                shader.SetUniformMat4f("u_MVP", mvp);
+                renderer.Draw(va, ib, shader);
+            }
 
             if (g > 1.0f)
                 increment = -0.5f;
@@ -151,7 +162,8 @@ int main(void)
             // render your GUI
             {
                 ImGui::Begin("Hello, World!");
-                ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 1080.0f);
+                ImGui::SliderFloat3("Translation 1", &translation1.x, 0.0f, 1080.0f);
+                ImGui::SliderFloat3("Translation 2", &translation2.x, 0.0f, 1080.0f);
                 ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
                 ImGui::End();
             }
@@ -170,7 +182,7 @@ int main(void)
 
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
+    //ImGui::DestroyContext();
 
     glfwTerminate();
     return 0;
