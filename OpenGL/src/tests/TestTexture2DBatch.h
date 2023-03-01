e3f1982bf75cfc6258230074424ef0ee5665df00
Jason Fong <jasonlmfong@gmail.com>
2023-03-01 08:26:28 -0500

    added batching

---
+++ OpenGL/src/tests/TestTexture2DBatch.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "Test.h"
+
+#include "VertexBuffer.h"
+#include "VertexBufferLayout.h"
+#include "Texture.h"
+
+namespace test {
+
+	class TestTexture2DBatch : public Test
+	{
+	public:
+		TestTexture2DBatch();
+		~TestTexture2DBatch();
+
+		void OnUpdate(float deltaTime) override;
+		void OnRender() override;
+		void OnImGuiRender() override;
+
+	private:
+		std::unique_ptr<VertexArray> m_VAO;
+		std::unique_ptr<VertexBuffer> m_VB;
+		std::unique_ptr<IndexBuffer> m_IB;
+		std::unique_ptr<Shader> m_Shader;
+		std::unique_ptr<Texture> m_Texture;
+
+		// MVP
+		glm::mat4 m_Proj, m_Model;
+
+		//translations
+		glm::vec3 m_Translation;
+
+		//color changing
+		float m_Green;
+		float m_Increment;
+	};
+
+}
