5e6320de73c0eb140a98cedfb30a1c35d77b9206
Jason Fong <jasonlmfong@gmail.com>
2023-03-04 15:27:18 -0500

    modulized the quad generation and rendered a grid

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -36,7 +36,7 @@ int main(void)
     if (!glfwInit())
         return -1;
 
-    // version 3.3 core
+    // version 4.5 core
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
--- OpenGL/src/tests/TestBatchDynamicGeometry.cpp
+++ OpenGL/src/tests/TestBatchDynamicGeometry.cpp
@@ -7,6 +7,8 @@
 #include "glm/gtc/matrix_transform.hpp"
 #include "Shader.h"
 
+#include <array>
+
 namespace test {
 
     struct Vertex
@@ -17,36 +19,40 @@ namespace test {
         float TextureID;
     };
 
-    static std::vector<Vertex> CreateQuad(float x, float y, 
+    static Vertex* CreateQuad(Vertex* target, float x, float y, 
         float tintRed, float tintGreen, float tintBlue, float tintA, float textureID)
     {
         float size = 100.0f;
 
-        Vertex v0{};
-        v0.Position = { x, y };
-        v0.TexCoord = { 0.0, 0.0 };
-        v0.Color = { tintRed, tintGreen, tintBlue, tintA };
-        v0.TextureID = textureID;
+        // add 1st vertex
+        target->Position = { x, y };
+        target->TexCoord = { 0.0, 0.0 };
+        target->Color = { tintRed, tintGreen, tintBlue, tintA };
+        target->TextureID = textureID;
+        target++;
 
-        Vertex v1{};
-        v1.Position = { x + size, y };
-        v1.TexCoord = { 1.0f, 0.0f };
-        v1.Color = { tintRed, tintGreen, tintBlue, tintA };
-        v1.TextureID = textureID;
+        // add 2nd vertex
+        target->Position = { x + size, y };
+        target->TexCoord = { 1.0f, 0.0f };
+        target->Color = { tintRed, tintGreen, tintBlue, tintA };
+        target->TextureID = textureID;
+        target++;
 
-        Vertex v2{};
-        v2.Position = { x + size, y + size };
-        v2.TexCoord = { 1.0f, 1.0f };
-        v2.Color = { tintRed, tintGreen, tintBlue, tintA };
-        v2.TextureID = textureID;
+        // add 3rd vertex
+        target->Position = { x + size, y + size };
+        target->TexCoord = { 1.0f, 1.0f };
+        target->Color = { tintRed, tintGreen, tintBlue, tintA };
+        target->TextureID = textureID;
+        target++;
 
-        Vertex v3{};
-        v3.Position = { x, y + size };
-        v3.TexCoord = { 0.0f, 1.0f };
-        v3.Color = { tintRed, tintGreen, tintBlue, tintA };
-        v3.TextureID = textureID;
+        // add 4th vertex
+        target->Position = { x, y + size };
+        target->TexCoord = { 0.0f, 1.0f };
+        target->Color = { tintRed, tintGreen, tintBlue, tintA };
+        target->TextureID = textureID;
+        target++;
 
-        return { v0, v1, v2, v3 };
+        return target;
     }
 
     TestBatchDynamicGeometry::TestBatchDynamicGeometry()
@@ -58,26 +64,13 @@ namespace test {
         GLCall(glEnable(GL_BLEND));
         GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 
-        /*float vertices[] = {
-            -50.0f, -50.0f, 0.0f, 0.0f, 0.18f, 0.60f, 0.96f, 1.0f, 0.0f, // 0
-             50.0f, -50.0f, 0.5f, 0.0f, 0.18f, 0.60f, 0.96f, 1.0f, 0.0f, // 1
-             50.0f,  50.0f, 0.5f, 1.0f, 0.18f, 0.60f, 0.96f, 1.0f, 0.0f, // 2
-            -50.0f,  50.0f, 0.0f, 1.0f, 0.18f, 0.60f, 0.96f, 1.0f, 0.0f, // 3
-
-            -50.0f, 250.0f, 0.0f, 0.0f, 0.91f, 0.26f, 0.21f, 1.0f, 0.0f, // 4
-             50.0f, 250.0f, 0.5f, 0.0f, 0.91f, 0.26f, 0.21f, 1.0f, 0.0f, // 5
-             50.0f, 350.0f, 0.5f, 1.0f, 0.91f, 0.26f, 0.21f, 1.0f, 0.0f, // 6
-            -50.0f, 350.0f, 0.0f, 1.0f, 0.91f, 0.26f, 0.21f, 1.0f, 0.0f, // 7
-
-            150.0f, 150.0f, 0.0f, 0.0f, 1.00f, 0.93f, 0.24f, 1.0f, 1.0f, // 8
-            250.0f, 150.0f, 1.0f, 0.0f, 1.00f, 0.93f, 0.24f, 1.0f, 1.0f, // 9
-            250.0f, 250.0f, 1.0f, 1.0f, 1.00f, 0.93f, 0.24f, 1.0f, 1.0f, // 10
-            150.0f, 250.0f, 0.0f, 1.0f, 1.00f, 0.93f, 0.24f, 1.0f, 1.0f  // 11
-        };*/
+        const size_t MaxQuadCount = 1000;
+        const size_t MaxVertexCount = MaxQuadCount * 4;
+        const size_t MaxIndexCount = MaxQuadCount * 6;
 
         m_VAO = std::make_unique<VertexArray>();
 
-        m_VB = std::make_unique<VertexBuffer>(nullptr, 12 * sizeof(Vertex), "dynamic");
+        m_VB = std::make_unique<VertexBuffer>(nullptr, MaxVertexCount * sizeof(Vertex), "dynamic");
         VertexBufferLayout layout;
         layout.Push<float>(2); // window coord
         layout.Push<float>(2); // texture coord
@@ -87,16 +80,31 @@ namespace test {
         // links the vertex array with the vertex buffer
         m_VAO->AddBuffer(*m_VB, layout);
 
-        unsigned int indices[] = {
+        /*unsigned int indices[] = {
             0, 1, 2,   // triangle 1
             2, 3, 0,   // triangle 2
             4, 5, 6,   // triangle 3
             6, 7, 4,   // triangle 4
             8, 9, 10,  // triangle 5
             10, 11, 8  // triangle 6
-        };
+        };*/
 
-        m_IB = std::make_unique<IndexBuffer>(indices, 6 * 3);
+        uint32_t indices[MaxIndexCount];
+        uint32_t offset = 0;
+        for (size_t i = 0; i < MaxIndexCount; i += 6)
+        {
+            indices[i + 0] = 0 + offset;
+            indices[i + 1] = 1 + offset;
+            indices[i + 2] = 2 + offset;
+
+            indices[i + 3] = 2 + offset;
+            indices[i + 4] = 3 + offset;
+            indices[i + 5] = 0 + offset;
+
+            offset += 4;
+        }
+
+        m_IB = std::make_unique<IndexBuffer>(indices, MaxIndexCount);
 
         m_Shader = std::make_unique<Shader>("res/shaders/Multi.shader");
         m_Shader->Bind();
@@ -136,21 +144,36 @@ namespace test {
             150.0f, 250.0f, 0.0f, 1.0f, 1.00f, 0.93f, 0.24f, 1.0f, 1.0f  // 11
         };*/
 
+        uint32_t indexCount = 0;
+
+        std::array<Vertex, 1000> vertices{};
+        Vertex* buffer = vertices.data();
+        
+        // draw grid with alternating textures
+        for (int y = 0; y < 500; y += 101)
+        {
+            for (int x = 0; x < 500; x += 101)
+            {
+                buffer = CreateQuad(buffer, x, y, 1.0f, 1.0f, 1.0f, 1.0f, (x + y) % 2);
+                indexCount += 6;
+            }
+        }
+
+        // blue tint penguin
+        buffer = CreateQuad(buffer, m_Quad0Position[0], m_Quad0Position[1], 0.18f, 0.60f, 0.96f, 1.0f, 0.0f);
+        indexCount += 6;
+        // red tint penguin
+        buffer = CreateQuad(buffer, m_Quad1Position[0], m_Quad1Position[1], 0.91f, 0.26f, 0.21f, 1.0f, 0.0f); 
+        indexCount += 6;
+        // yellow tint icon
+        buffer = CreateQuad(buffer, m_Quad2Position[0], m_Quad2Position[1], 1.00f, 0.93f, 0.24f, 1.0f, 1.0f);
+        indexCount += 6;
+
         m_VB->Bind();
-
-        std::vector<Vertex> q0 = CreateQuad(m_Quad0Position[0], m_Quad0Position[1], 0.18f, 0.60f, 0.96f, 1.0f, 0.0f);
-        std::vector<Vertex> q1 = CreateQuad(m_Quad1Position[0], m_Quad1Position[1], 0.91f, 0.26f, 0.21f, 1.0f, 0.0f);
-        std::vector<Vertex> q2 = CreateQuad(m_Quad2Position[0], m_Quad2Position[1], 1.00f, 0.93f, 0.24f, 1.0f, 1.0f);
-
-        Vertex vertices[12]{};
-        memcpy(vertices, q0.data(), q0.size() * sizeof(Vertex));
-        memcpy(vertices + q0.size(), q1.data(), q1.size() * sizeof(Vertex));
-        memcpy(vertices + 8, q2.data(), q2.size() * sizeof(Vertex));
-
         // load data into vertex buffer
         /*glMapBuffer();
         glUnmapBuffer();*/
-        GLCall(glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices));
+        GLCall(glBufferSubData(GL_ARRAY_BUFFER, 0, vertices.size() * sizeof(Vertex), vertices.data()));
 
         GLCall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
         GLCall(glClear(GL_COLOR_BUFFER_BIT));
