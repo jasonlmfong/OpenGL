b81cebc9a87b093e87c2b9c1fc3ca745b165492b
Jason Fong <jasonlmfong@gmail.com>
2023-03-05 21:11:41 -0500

    fixed batch rendering black sreen bug!

--- OpenGL/src/tests/TestBatchRendering.cpp
+++ OpenGL/src/tests/TestBatchRendering.cpp
@@ -3,15 +3,13 @@
 #include "stb_image/stb_image.h"
 
 #include "Renderer.h"
-#include "BatchRenderer.h"
+#include "VertexBufferLayout.h"
 #include "imgui/imgui.h"
 
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include <glm/gtc/type_ptr.hpp>
 #include "Shader.h"
-
-#include <array>
 
 namespace test {
 
@@ -42,9 +40,14 @@ namespace test {
 
     TestBatchRendering::TestBatchRendering()
         : m_Proj(glm::ortho(0.0f, 1920.0f, 0.0f, 1080.0f, -1.0f, 1.0f)),
-        m_Model(glm::rotate(glm::mat4(1.0f), glm::radians(20.0f), glm::vec3(0.0f, 0.0f, 1.0f))),
-        m_Translation(300, 200, 0)
+        m_Model(glm::rotate(glm::mat4(1.0f), glm::radians(0.0f), glm::vec3(0.0f, 0.0f, 1.0f))),
+        m_Translation(0, 0, 0),
+        m_IndexCount(0), m_QuadBuffer(nullptr), m_QuadBufferPtr(nullptr), 
+        m_TextureSlotIndex(1)
     {
+        // statistics
+        Stats m_RenderStats;
+
         m_Shader = std::make_unique<Shader>("res/shaders/BatchRender.shader");
         m_Shader->Bind();
 
@@ -53,9 +56,51 @@ namespace test {
             samplers[i] = i;
         m_Shader->SetUniform1iv("u_Textures", 32, samplers);
 
-        GLCall(glClearColor(0.1f, 0.1f, 0.1f, 1.0f));
+        m_QuadBuffer = new Vertex[MaxVertexCount];
 
-        BatchRenderer::Init();
+        m_VAO = std::make_unique<VertexArray>();
+
+        m_VB = std::make_unique<VertexBuffer>(nullptr, MaxVertexCount * sizeof(Vertex), "dynamic");
+
+        VertexBufferLayout layout;
+        layout.Push<float>(2); // window coord
+        layout.Push<float>(2); // texture coord
+        layout.Push<float>(4); // color
+        layout.Push<float>(1); // texture ID
+        // links the vertex array with the vertex buffer
+        m_VAO->AddBuffer(*m_VB, layout);
+
+        uint32_t indices[MaxIndexCount]{};
+        uint32_t offset = 0;
+        for (size_t i = 0; i < MaxIndexCount; i += 6)
+        {
+            indices[i + 0] = 0 + offset;
+            indices[i + 1] = 1 + offset;
+            indices[i + 2] = 2 + offset;
+
+            indices[i + 3] = 2 + offset;
+            indices[i + 4] = 3 + offset;
+            indices[i + 5] = 0 + offset;
+
+            offset += 4;
+        }
+
+        m_IB = std::make_unique<IndexBuffer>(indices, MaxIndexCount);
+
+
+        // 1x1 white texture
+        glCreateTextures(GL_TEXTURE_2D, 1, &m_TextureWhite);
+        glBindTexture(GL_TEXTURE_2D, m_TextureWhite);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        uint32_t color = 0xffffffff;
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, 1, 1, 0, GL_RGBA, GL_UNSIGNED_BYTE, &color);
+
+        m_TextureSlots[0] = m_TextureWhite;
+        for (size_t i = 1; i < MaxTextures; i++)
+            m_TextureSlots[i] = 0;
 
         // load texture
         m_Texture1 = LoadTexture("res/textures/Penguin.png");
@@ -64,7 +109,6 @@ namespace test {
 
     TestBatchRendering::~TestBatchRendering()
     {
-        BatchRenderer::Shutdown();
     }
 
     void TestBatchRendering::OnUpdate(float deltaTime)
@@ -82,8 +126,8 @@ namespace test {
         glm::mat4 mvp = m_Proj * view * m_Model;
         m_Shader->SetUniformMat4f("u_MVP", mvp);
 
-        BatchRenderer::ResetStats();
-        BatchRenderer::BeginBatch();
+        TestBatchRendering::ResetStats();
+        TestBatchRendering::BeginBatch();
 
         // draw background
         for (float y = 0.0f; y < 1080.0f; y += 10.0f)
@@ -91,7 +135,7 @@ namespace test {
             for (float x = 0.0f; x < 1080.0f; x += 10.0f)
             {
                 glm::vec4 color = { (x / 108.0f), 0.2f, (y / 108.0f), 1.0f };
-                BatchRenderer::DrawQuad({ x, y }, { 9.0f, 9.0f }, color);
+                TestBatchRendering::DrawQuad({ x, y }, { 9.0f, 9.0f }, color);
             }
         }
 
@@ -101,24 +145,162 @@ namespace test {
             for (int x = 0; x < 500; x += 101)
             {
                 unsigned int tex = (x + y) % 2 == 0 ? m_Texture1 : m_Texture2;
-                BatchRenderer::DrawQuad({ x, y }, { 100.0f, 100.0f }, tex);
+                TestBatchRendering::DrawQuad({ x, y }, { 100.0f, 100.0f }, tex);
             }
         }
 
         // penguin
-        BatchRenderer::DrawQuad(m_QuadPosition, { 200.0f, 200.0f }, m_Texture1);
-        BatchRenderer::EndBatch();
+        TestBatchRendering::DrawQuad(m_Quad1Position, { 200.0f, 200.0f }, m_Texture1);
+        // icon
+        TestBatchRendering::DrawQuad(m_Quad2Position, { 450.0f, 450.0f }, m_Texture2);
 
-        BatchRenderer::Flush();
+        TestBatchRendering::EndBatch();
+
+        TestBatchRendering::Flush();
     }
 
     void TestBatchRendering::OnImGuiRender()
     {
         ImGui::SliderFloat3("Translation", &m_Translation.x, 0.0f, 1080.0f);
-        ImGui::DragFloat2("Quad Position", &m_QuadPosition[0], 1.0f);
-        ImGui::Text("Quads: %d", BatchRenderer::GetStats().QuadCount);
-        ImGui::Text("Draws: %d", BatchRenderer::GetStats().DrawCount);
+        ImGui::DragFloat2("Quad 1 Position", &m_Quad1Position[0], 1.0f);
+        ImGui::DragFloat2("Quad 2 Position", &m_Quad2Position[0], 1.0f);
+        ImGui::Text("Quads: %d", TestBatchRendering::GetStats().QuadCount);
+        ImGui::Text("Draws: %d", TestBatchRendering::GetStats().DrawCount);
         ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+    }
+
+    void TestBatchRendering::BeginBatch()
+    {
+        m_QuadBufferPtr = m_QuadBuffer;
+    }
+
+    void TestBatchRendering::EndBatch()
+    {
+        GLsizeiptr size = (uint8_t*)m_QuadBufferPtr - (uint8_t*)m_QuadBuffer;
+        m_VB->Bind();
+        glBufferSubData(GL_ARRAY_BUFFER, 0, size, m_QuadBuffer);
+    }
+
+    void TestBatchRendering::Flush()
+    {
+        for (uint32_t i = 0; i < m_TextureSlotIndex; i++)
+            glBindTextureUnit(i, m_TextureSlots[i]);
+
+        m_VAO->Bind();
+        glDrawElements(GL_TRIANGLES, m_IndexCount, GL_UNSIGNED_INT, nullptr);
+        m_RenderStats.DrawCount++;
+
+        m_IndexCount = 0;
+        m_TextureSlotIndex = 1;
+    }
+
+    void TestBatchRendering::DrawQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4& color)
+    {
+        if (m_IndexCount >= MaxIndexCount)
+        {
+            EndBatch();
+            Flush();
+            BeginBatch();
+        }
+
+        // default no texture for pure color rendering
+        float textureIndex = 0.0f;
+
+        m_QuadBufferPtr->Position = { position.x, position.y };
+        m_QuadBufferPtr->TexCoord = { 0.0f, 0.0f };
+        m_QuadBufferPtr->Color = color;
+        m_QuadBufferPtr->TextureID = textureIndex;
+        m_QuadBufferPtr++;
+
+        m_QuadBufferPtr->Position = { position.x + size.x, position.y };
+        m_QuadBufferPtr->TexCoord = { 1.0f, 0.0f };
+        m_QuadBufferPtr->Color = color;
+        m_QuadBufferPtr->TextureID = textureIndex;
+        m_QuadBufferPtr++;
+
+        m_QuadBufferPtr->Position = { position.x + size.x, position.y + size.y };
+        m_QuadBufferPtr->TexCoord = { 1.0f, 1.0f };
+        m_QuadBufferPtr->Color = color;
+        m_QuadBufferPtr->TextureID = textureIndex;
+        m_QuadBufferPtr++;
+
+        m_QuadBufferPtr->Position = { position.x, position.y + size.y };
+        m_QuadBufferPtr->TexCoord = { 0.0f, 1.0f };
+        m_QuadBufferPtr->Color = color;
+        m_QuadBufferPtr->TextureID = textureIndex;
+        m_QuadBufferPtr++;
+
+        m_IndexCount += 6;
+        m_RenderStats.QuadCount++;
+    }
+
+    void TestBatchRendering::DrawQuad(const glm::vec2& position, const glm::vec2& size, const uint32_t textureID)
+    {
+        if (m_IndexCount >= MaxIndexCount || m_TextureSlotIndex > 31)
+        {
+            EndBatch();
+            Flush();
+            BeginBatch();
+        }
+
+        // default no tint for texture
+        constexpr glm::vec4 color = { 1.0f, 1.0f, 1.0f, 1.0f };
+
+        // check if hte texture is used
+        float textureIndex = 0.0f;
+        for (uint32_t i = 1; i < m_TextureSlotIndex; i++)
+        {
+            if (m_TextureSlots[i] == textureID)
+            {
+                textureIndex = (float)i;
+                break;
+            }
+        }
+
+        // if texture has not been used, save it
+        if (textureIndex == 0.0f)
+        {
+            textureIndex = (float)m_TextureSlotIndex;
+            m_TextureSlots[m_TextureSlotIndex] = textureID;
+            m_TextureSlotIndex++;
+        }
+
+        m_QuadBufferPtr->Position = { position.x, position.y };
+        m_QuadBufferPtr->TexCoord = { 0.0f, 0.0f };
+        m_QuadBufferPtr->Color = color;
+        m_QuadBufferPtr->TextureID = textureIndex;
+        m_QuadBufferPtr++;
+
+        m_QuadBufferPtr->Position = { position.x + size.x, position.y };
+        m_QuadBufferPtr->TexCoord = { 1.0f, 0.0f };
+        m_QuadBufferPtr->Color = color;
+        m_QuadBufferPtr->TextureID = textureIndex;
+        m_QuadBufferPtr++;
+
+        m_QuadBufferPtr->Position = { position.x + size.x, position.y + size.y };
+        m_QuadBufferPtr->TexCoord = { 1.0f, 1.0f };
+        m_QuadBufferPtr->Color = color;
+        m_QuadBufferPtr->TextureID = textureIndex;
+        m_QuadBufferPtr++;
+
+        m_QuadBufferPtr->Position = { position.x, position.y + size.y };
+        m_QuadBufferPtr->TexCoord = { 0.0f, 1.0f };
+        m_QuadBufferPtr->Color = color;
+        m_QuadBufferPtr->TextureID = textureIndex;
+        m_QuadBufferPtr++;
+
+        m_IndexCount += 6;
+        m_RenderStats.QuadCount++;
+    }
+
+    const test::TestBatchRendering::Stats test::TestBatchRendering::GetStats()
+    {
+        return m_RenderStats;
+    }
+
+    void TestBatchRendering::ResetStats()
+    {
+        memset(&m_RenderStats, 0, sizeof(Stats));
     }
 
 }
--- OpenGL/src/tests/TestBatchRendering.h
+++ OpenGL/src/tests/TestBatchRendering.h
@@ -3,12 +3,32 @@
 #include "Test.h"
 
 #include "Texture.h"
+#include <array>
+
+static const size_t MaxQuadCount = 1000;
+static const size_t MaxVertexCount = MaxQuadCount * 4;
+static const size_t MaxIndexCount = MaxQuadCount * 6;
+static const size_t MaxTextures = 32;
 
 namespace test {
 
 	class TestBatchRendering : public Test
 	{
 	public:
+		struct Vertex
+		{
+			glm::vec2 Position;
+			glm::vec2 TexCoord;
+			glm::vec4 Color;
+			float TextureID;
+		};
+
+		struct Stats
+		{
+			uint32_t DrawCount = 0;
+			uint32_t QuadCount = 0;
+		};
+
 		TestBatchRendering();
 		~TestBatchRendering();
 
@@ -16,9 +36,22 @@ namespace test {
 		void OnRender() override;
 		void OnImGuiRender() override;
 
+		void BeginBatch();
+		void EndBatch();
+		void Flush();
+
+		void DrawQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4& color);
+		void DrawQuad(const glm::vec2& position, const glm::vec2& size, const uint32_t textureID);
+
+		const TestBatchRendering::Stats GetStats();
+		void ResetStats();
+
 	private:
+		std::unique_ptr<VertexArray> m_VAO;
+		std::unique_ptr<VertexBuffer> m_VB;
+		std::unique_ptr<IndexBuffer> m_IB;
 		std::unique_ptr<Shader> m_Shader;
-		unsigned int m_Texture1, m_Texture2;
+		unsigned int m_TextureWhite, m_Texture1, m_Texture2;
 
 		// MVP
 		glm::mat4 m_Proj, m_Model;
@@ -26,7 +59,20 @@ namespace test {
 		//translations
 		glm::vec3 m_Translation;
 
-		glm::vec2 m_QuadPosition = { 100.0f, 100.0f };
+		glm::vec2 m_Quad1Position = { 100.0f, 100.0f };
+		glm::vec2 m_Quad2Position = { 350.0f, 350.0f };
+
+		uint32_t m_IndexCount = 0;
+
+		Vertex* m_QuadBuffer = nullptr;
+		Vertex* m_QuadBufferPtr = nullptr;
+
+		std::array<uint32_t, MaxTextures> m_TextureSlots;
+		uint32_t m_TextureSlotIndex = 1;
+
+		// statistics
+		
+		Stats m_RenderStats;
 	};
 
 }
